62c5d3da65e7aba07b42caedba5121b7f3908a8c
white <whitestar0713@gmail.com>
2022-09-29 08:19:41 -0600

    first react-native app

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    
+      <Text style={{backgroundColor:black}}>
+        Try editing me! 🎉
+      </Text>
+    
+  );
+}
+
+export default App;
